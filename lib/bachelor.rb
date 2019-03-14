ebab5c2f0929540b74a24e436d145de7a75213d0
Jeff M <jkdmarks@gmail.com>
2019-03-14 01:44:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,28 +1,51 @@
 require 'pry'
 
+
 def get_first_name_of_season_winner(data, season)
   winner_name = ""
+  
   data.each do |season_num, contestants|
-    binding.pry
-    if :"#{season}" == season_num
+    if season.to_s == season_num.to_s
       for contestant in contestants
-        if contestant[:status] == "Winner"
-          winner_name += contestant[:name].split(" ")[0]
+        if contestant["status"] == "Winner"
+          winner_name = contestant["name"].split(" ")[0]
         end
       end
     end
   end
+
   winner_name
 end
 
 
-# def get_contestant_name(data, occupation)
-#   # code here
-# end
+def get_contestant_name(data, occupation)
+  name = ""
 
-# def count_contestants_by_hometown(data, hometown)
-#   # code here
-# end
+  data.each do |season, contestants|
+    contestants.each do |attribute|      
+      if attribute["occupation"] == occupation
+        name = attribute["name"]
+      end
+    end
+  end
+
+  name
+end
+
+
+def count_contestants_by_hometown(data, hometown)
+  count = 0
+
+  data.each do |season, contestants|
+    contestants.each do |attribute|      
+      if attribute["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+
+  count
+end
 
 # def get_occupation(data, hometown)
 #   # code here
